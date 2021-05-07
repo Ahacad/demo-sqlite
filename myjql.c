8826f2f33009dfb0b31a3de8029dee2c84e52509
ahacad <ahacad@126.com>
2021-05-07 21:11:05 +0800

    defs for nodes

--- myjql.c
+++ myjql.c
@@ -12,37 +12,88 @@
 #include <string.h>
 #include <unistd.h>
 
+/*
+ *table
+ */
 #define COLUMN_B_SIZE 11
 #define TABLE_MAX_PAGES 500
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 const uint32_t PAGE_SIZE = 4096;
 // FIXME: test whether 500 is enough
-
 // the table struct specified in the PJ
 typedef struct {
     uint32_t a;
     char b[COLUMN_B_SIZE + 1];
 } Row;
-
+const uint32_t ROW_SIZE = size_of_attribute(Row, a) + size_of_attribute(Row, b);
 typedef struct {
     int file_descriptor;
     uint32_t file_length;
     uint32_t num_pages;
     void* pages[TABLE_MAX_PAGES];
 } Pager;
-
 typedef struct {
     Pager* pager;
     uint32_t root_page_num;
 } Table;
-
 typedef struct {
     Table* table;
     uint32_t page_num;
     uint32_t cell_num;
     bool is_end_of_table;
 } Cursor;
-
 Table table;
+
+/*
+ *nodes
+ */
+
+typedef enum { NODE_INTERNAL, NODE_LEAF } NodeType;
+
+// common header
+const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
+const uint32_t NODE_TYPE_OFFSET = 0;
+const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
+const uint32_t IS_ROOT_OFFSET = NODE_TYPE_SIZE;
+const uint32_t PARENT_POINTER_SIZE = sizeof(uint32_t);
+const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
+const uint32_t NODE_HEADER_SIZE =
+    NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
+// internal node header
+const uint32_t INTERNAL_NODE_NUM_KEYS_SIZE = sizeof(uint32_t);
+const uint32_t INTERNAL_NODE_NUM_KEYS_OFFSET = NODE_HEADER_SIZE;
+const uint32_t INTERNAL_NODE_RIGHT_CHILD_SIZE = sizeof(uint32_t);
+const uint32_t INTERNAL_NODE_RIGHT_CHILD_OFFSET =
+    INTERNAL_NODE_NUM_KEYS_OFFSET + INTERNAL_NODE_NUM_KEYS_SIZE;
+const uint32_t INTERNAL_NODE_HEADER_SIZE = NODE_HEADER_SIZE +
+                                           INTERNAL_NODE_NUM_KEYS_SIZE +
+                                           INTERNAL_NODE_RIGHT_CHILD_SIZE;
+// internal node body
+const uint32_t INTERNAL_NODE_KEY_SIZE = sizeof(uint32_t);
+const uint32_t INTERNAL_NODE_CHILD_SIZE = sizeof(uint32_t);
+const uint32_t INTERNAL_NODE_CELL_SIZE =
+    INTERNAL_NODE_CHILD_SIZE + INTERNAL_NODE_KEY_SIZE;
+// leaf node header
+const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_NEXT_LEAF_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_NEXT_LEAF_OFFSET =
+    LEAF_NODE_NUM_CELLS_OFFSET + LEAF_NODE_NUM_CELLS_SIZE;
+const uint32_t LEAF_NODE_HEADER_SIZE =
+    NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_OFFSET + LEAF_NODE_NEXT_LEAF_SIZE;
+// leaf body
+const uint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_KEY_OFFSET = 0;
+const uint32_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
+const uint32_t LEAF_NODE_VALUE_OFFSET =
+    LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE;
+const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
+const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_MAX_CELLS =
+    LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
+const uint32_t LEAF_NODE_RIGHT_SPLIT_COUNT = (LEAF_NODE_MAX_CELLS + 1) / 2;
+const uint32_t LEAF_NODE_LEFT_SPLIT_COUNT =
+    (LEAF_NODE_MAX_CELLS + 1) - LEAF_NODE_RIGHT_SPLIT_COUNT;
 
 /* shell IO */
 
@@ -136,8 +187,7 @@ void open_file(const char* filename) { /* open file */
 
     if (pager->num_pages == 0) {
         // new table
-        // TODO: new table
-        /*void* root_node = get_page(pager,0);*/
+        void* root_node = get_page(pager, 0);
         /*initialize_leaf_node(root_node);*/
         /*set_onde_root(root_node, true);*/
     }
@@ -172,8 +222,6 @@ struct {
 } statement;
 
 /* B-Tree operations */
-
-typedef enum { NODE_INTERNAL, NODE_LEAF } NodeType;
 
 /* leaf node body */
 
